1934302b4a49af86031181ec9f0044eed46e2193
Daniel Hölbling <29107418+iths-daniel-holbling@users.noreply.github.com>
2019-10-08 17:07:51 +0200

    Variant with user set number to display

---
+++ usercardAnyNumber/script.js
@@ -0,0 +1,103 @@
+// Parsing
+let pd = JSON.parse('{"results":[{"name":{"title":"Miss","first":"Jasmine","last":"Singh"},"email":"Jasmine.Singh@example.com","phone":"769-904-6220","picture":{"large":"https://randomuser.me/api/portraits/women/85.jpg","medium":"https://randomuser.me/api/portraits/med/women/85.jpg","thumbnail":"https://randomuser.me/api/portraits/thumb/women/85.jpg"}},{"name":{"title":"Mrs","first":"Chloe","last":"Zhang"},"email":"Chloe.Zhang@example.com","phone":"(995)-968-0405","picture":{"large":"https://randomuser.me/api/portraits/women/93.jpg","medium":"https://randomuser.me/api/portraits/med/women/93.jpg","thumbnail":"https://randomuser.me/api/portraits/thumb/women/93.jpg"}},{"name":{"title":"Mr","first":"عباس","last":"زارعی"},"email":"aabs.zraay@example.com","phone":"066-28761041","picture":{"large":"https://randomuser.me/api/portraits/men/37.jpg","medium":"https://randomuser.me/api/portraits/med/men/37.jpg","thumbnail":"https://randomuser.me/api/portraits/thumb/men/37.jpg"}},{"name":{"title":"Mrs","first":"Pinja","last":"Autio"},"email":"Pinja.Autio@example.com","phone":"03-164-484","picture":{"large":"https://randomuser.me/api/portraits/women/5.jpg","medium":"https://randomuser.me/api/portraits/med/women/5.jpg","thumbnail":"https://randomuser.me/api/portraits/thumb/women/5.jpg"}},{"name":{"title":"Miss","first":"Rachel","last":"Lemaire"},"email":"Rachel.Lemaire@example.com","phone":"02-91-22-31-25","picture":{"large":"https://randomuser.me/api/portraits/women/44.jpg","medium":"https://randomuser.me/api/portraits/med/women/44.jpg","thumbnail":"https://randomuser.me/api/portraits/thumb/women/44.jpg"}},{"name":{"title":"Miss","first":"Viivi","last":"Mikkola"},"email":"Viivi.Mikkola@example.com","phone":"03-675-390","picture":{"large":"https://randomuser.me/api/portraits/women/64.jpg","medium":"https://randomuser.me/api/portraits/med/women/64.jpg","thumbnail":"https://randomuser.me/api/portraits/thumb/women/64.jpg"}},{"name":{"title":"Ms","first":"آوا","last":"مرادی"},"email":"aw.mrdy@example.com","phone":"069-22294050","picture":{"large":"https://randomuser.me/api/portraits/women/51.jpg","medium":"https://randomuser.me/api/portraits/med/women/51.jpg","thumbnail":"https://randomuser.me/api/portraits/thumb/women/51.jpg"}},{"name":{"title":"Mr","first":"Eckhart","last":"Kirsten"},"email":"Eckhart.Kirsten@example.com","phone":"0749-3852718","picture":{"large":"https://randomuser.me/api/portraits/men/34.jpg","medium":"https://randomuser.me/api/portraits/med/men/34.jpg","thumbnail":"https://randomuser.me/api/portraits/thumb/men/34.jpg"}},{"name":{"title":"Mr","first":"Birkan","last":"Van der Stroom"},"email":"Birkan.VanderStroom@example.com","phone":"(183)-737-2106","picture":{"large":"https://randomuser.me/api/portraits/men/63.jpg","medium":"https://randomuser.me/api/portraits/med/men/63.jpg","thumbnail":"https://randomuser.me/api/portraits/thumb/men/63.jpg"}},{"name":{"title":"Mr","first":"Dan","last":"Niebuhr"},"email":"Dan.Niebuhr@example.com","phone":"0569-7016684","picture":{"large":"https://randomuser.me/api/portraits/men/85.jpg","medium":"https://randomuser.me/api/portraits/med/men/85.jpg","thumbnail":"https://randomuser.me/api/portraits/thumb/men/85.jpg"}},{"name":{"title":"Mrs","first":"Andrea","last":"Mckinney"},"email":"Andrea.Mckinney@example.com","phone":"071-502-1842","picture":{"large":"https://randomuser.me/api/portraits/women/40.jpg","medium":"https://randomuser.me/api/portraits/med/women/40.jpg","thumbnail":"https://randomuser.me/api/portraits/thumb/women/40.jpg"}},{"name":{"title":"Mr","first":"Derrick","last":"Weaver"},"email":"Derrick.Weaver@example.com","phone":"(711)-884-5832","picture":{"large":"https://randomuser.me/api/portraits/men/91.jpg","medium":"https://randomuser.me/api/portraits/med/men/91.jpg","thumbnail":"https://randomuser.me/api/portraits/thumb/men/91.jpg"}},{"name":{"title":"Madame","first":"Léa","last":"Simon"},"email":"Lea.Simon@example.com","phone":"077 320 47 89","picture":{"large":"https://randomuser.me/api/portraits/women/50.jpg","medium":"https://randomuser.me/api/portraits/med/women/50.jpg","thumbnail":"https://randomuser.me/api/portraits/thumb/women/50.jpg"}},{"name":{"title":"Mr","first":"Raimundo","last":"Barros"},"email":"Raimundo.Barros@example.com","phone":"(55) 7965-7321","picture":{"large":"https://randomuser.me/api/portraits/men/77.jpg","medium":"https://randomuser.me/api/portraits/med/men/77.jpg","thumbnail":"https://randomuser.me/api/portraits/thumb/men/77.jpg"}},{"name":{"title":"Mr","first":"Eren","last":"Sözeri"},"email":"Eren.Sozeri@example.com","phone":"(132)-689-0006","picture":{"large":"https://randomuser.me/api/portraits/men/47.jpg","medium":"https://randomuser.me/api/portraits/med/men/47.jpg","thumbnail":"https://randomuser.me/api/portraits/thumb/men/47.jpg"}},{"name":{"title":"Miss","first":"Coline","last":"Noel"},"email":"Coline.Noel@example.com","phone":"01-89-44-13-44","picture":{"large":"https://randomuser.me/api/portraits/women/61.jpg","medium":"https://randomuser.me/api/portraits/med/women/61.jpg","thumbnail":"https://randomuser.me/api/portraits/thumb/women/61.jpg"}},{"name":{"title":"Monsieur","first":"Carl","last":"Giraud"},"email":"Carl.Giraud@example.com","phone":"076 376 36 50","picture":{"large":"https://randomuser.me/api/portraits/men/37.jpg","medium":"https://randomuser.me/api/portraits/med/men/37.jpg","thumbnail":"https://randomuser.me/api/portraits/thumb/men/37.jpg"}},{"name":{"title":"Ms","first":"Sophie","last":"Turner"},"email":"Sophie.Turner@example.com","phone":"(064)-788-4604","picture":{"large":"https://randomuser.me/api/portraits/women/53.jpg","medium":"https://randomuser.me/api/portraits/med/women/53.jpg","thumbnail":"https://randomuser.me/api/portraits/thumb/women/53.jpg"}},{"name":{"title":"Mr","first":"Eric","last":"Mai"},"email":"Eric.Mai@example.com","phone":"0869-2701517","picture":{"large":"https://randomuser.me/api/portraits/men/89.jpg","medium":"https://randomuser.me/api/portraits/med/men/89.jpg","thumbnail":"https://randomuser.me/api/portraits/thumb/men/89.jpg"}},{"name":{"title":"Miss","first":"Rukiye","last":"Van der Kruk"},"email":"Rukiye.VanderKruk@example.com","phone":"(996)-772-9008","picture":{"large":"https://randomuser.me/api/portraits/women/67.jpg","medium":"https://randomuser.me/api/portraits/med/women/67.jpg","thumbnail":"https://randomuser.me/api/portraits/thumb/women/67.jpg"}}],"info":{"seed":"65d89dd7259a8619","results":20,"page":1,"version":"1.3"}}').results;
+
+// Set the number to display
+let numberToDisplay = 3;
+
+// Variables
+let userDataRow = 0;
+let wrapper = document.querySelector('.card-wrapper');
+let leftButton = document.querySelector('#left');
+let rightButton = document.querySelector('#right');
+let displayArray = [];
+let lastAction = ""
+
+// Functions
+const createCard = function(person){
+    // Card wrapper
+    let card = document.createElement('div');
+    card.className = 'card';
+    wrapper.appendChild(card);
+
+    // Top section
+    let topSection = document.createElement('div');
+    topSection.className = 'top-section';
+    card.appendChild(topSection);
+    let name = document.createElement('h2');
+    name.className = 'name';
+    name.innerText = person.name.title+" "+person.name.first+" "+person.name.last;
+    topSection.appendChild(name);
+
+    // Mid section
+    let midSectionHolder = document.createElement('div');
+    midSectionHolder.className = 'mid-section-holder';
+    card.appendChild(midSectionHolder);
+    let midSection = document.createElement('div');
+    midSection.className = 'mid-section';
+    midSection.style.backgroundImage = "url(" + person.picture.large + ")";
+    midSectionHolder.appendChild(midSection);
+
+    //Bottom section
+    let bottomSection = document.createElement('div');
+    bottomSection.className = 'bottom-section';
+    card.appendChild(bottomSection);
+    let emailLabel = document.createElement('h3');
+    emailLabel.className = 'label';
+    emailLabel.innerText= 'e-mail';
+    bottomSection.appendChild(emailLabel);
+    let email = document.createElement('h3');
+    email.className = 'text email';
+    email.innerText = person.email;
+    bottomSection.appendChild(email);
+    let phoneLabel = document.createElement('h3');
+    phoneLabel.classList = 'label';
+    phoneLabel.innerText = 'phone';
+    bottomSection.appendChild(phoneLabel);
+    let phone = document.createElement('h3');
+    phone.classList = 'text phone';
+    phone.innerText = person.phone;
+    bottomSection.appendChild(phone);
+}
+
+// INIT
+for(i = 0; i < numberToDisplay; i++){
+    displayArray.push(i);
+    createCard(pd[i]);
+}
+
+// EVENT LISTENERS
+leftButton.addEventListener("click", function(event){
+    let tempArray = [];
+    displayArray.forEach(function(number){
+        if(number == 0){
+            number = pd.length-1;
+            tempArray.push(number)
+        }else{
+            number--
+            tempArray.push(number)
+        }
+    })
+    displayArray = tempArray;
+    wrapper.innerHTML = "";
+    for(i = 0; i < displayArray.length; i++){
+        createCard(pd[displayArray[i]]);
+    }
+})
+
+rightButton.addEventListener("click", function(event){
+    let tempArray = [];
+    displayArray.forEach(function(number){
+        if(number == pd.length-1){
+            number = 0;
+            tempArray.push(number)
+        }else{
+            number++
+            tempArray.push(number)
+        }
+    })
+    displayArray = tempArray;
+    wrapper.innerHTML = "";
+    for(i = 0; i < displayArray.length; i++){
+        createCard(pd[displayArray[i]]);
+    }
+})
